bug fix average utility

backend.py
@@ -243,26 +243,39 @@ class Player:
         self.public_reputation = 1  # Public reputation as a discrete value (-1 or 1)
         self.neighbors = []  # List of neighboring players
         self.recent_utilities = []  # Utilities from the last x rounds
-        self.max_recent_rounds = 20  # Maximum number of recent rounds to track
+        self.interactions_per_round = []
+        self.max_recent_rounds = 10  # Maximum number of recent rounds to track
         self.all_utilities = []
+        self.times_asked = 0 
 
     def update_utility(self, utility_change):
         self.total_utility += utility_change
         self.recent_utilities.append(utility_change)
         self.all_utilities.append(utility_change)
 
+        '''
         # Keep only the last x rounds
-        if len(self.recent_utilities) > self.max_recent_rounds:
-            self.recent_utilities.pop(0)
+        if len(self.recent_utilities) > total_interaction:
+            self.recent_utilities.pop(0)'''
+
+    def update_interactions_per_round(self):
+        self.interactions_per_round.append(self.times_asked + 1)
+        self.times_asked = 0
+        
+        if len(self.interactions_per_round) > self.max_recent_rounds:
+            self.recent_utilities = self.recent_utilities[self.interactions_per_round[0]:]
+            self.interactions_per_round = self.interactions_per_round[1:]
+            
 
     def update_reputation(self, reputation_change, max_reputation=1, min_reputation=-1):
         self.real_reputation = min(max_reputation, max(min_reputation, self.real_reputation + reputation_change))
         self.public_reputation = max_reputation if self.real_reputation >= 0 else min_reputation
 
     def get_average_utility_per_round(self):
-        if len(self.recent_utilities) == 0:
+        if not (self.recent_utilities and self.interactions_per_round):
             return 0
-        return np.mean(self.recent_utilities)*2 # multiply by 2 to get the average utility per round instead of per favor_change
+        avg_utility = sum(self.recent_utilities)/len(self.interactions_per_round)
+        return avg_utility # multiply by 2 to get the average utility per round instead of per favor_change
     
     def decide_ask_for_help(self, asking_style, prob_power):
         return self.strategy.ask_for_help(self, self.neighbors, asking_style, prob_power)
@@ -381,6 +394,11 @@ class Game:
                     target.update_utility(utility_responder)
                     #self.reputation_manager.update_reputation(player, "reject", favor_size)
                     self.reputation_manager.update_reputation(player, target, "reject", favor_size)
+                target.times_asked += 1
+            else:
+                player.update_utility(0)
+        for player in players_in_order:
+            player.update_interactions_per_round()
 
     def play_rounds(self, num_rounds):
         for _ in range(num_rounds):

main.py
@@ -23,7 +23,7 @@ class SimpleUtility(UtilityFunction):
         if action == "cooperate":
             return favor_size, -favor_size/2
         elif action == "reject":
-            return -favor_size/3, 0
+            return 0, 0
         else:  # No action
             return 0, 0
         
@@ -125,17 +125,16 @@ if __name__ == "__main__":
     plot_grid_player_and_neighbors(grid, player_id=23)
     
     
-    """powers = np.linspace(1.5,1.6,10)
+    """powers = [0.5]
     print(powers)
-    moral_score = np.zeros((10,2))
     for i, power in enumerate(powers):
         random.seed(10)
         grid.setup_random()
         print(power)
-        game = Game(grid, SimpleUtility(), ReputationManager(gain_base=0.1, loss_base=0.01), asking_style = "distributed", prob_power=power) ## Choose and asking_style between "random", "best" and "distributed"
+        game = Game(grid, SimpleUtility(), ReputationManager(gain_base=0.1, loss_base=0.1), asking_style = "distributed", prob_power=power) ## Choose and asking_style between "random", "best" and "distributed"
 
         evolution = Evolution(game, inverse_copy_prob=60, inverse_mutation_prob=1000, inverse_pardon_prob=200, random_mutation=True)
-        evolution.run_interactive(record_data = True, plotting_frequenz=1000)
+        evolution.run_evolution(rounds = 5000)
         evolution.plot_history(i)
         evolution.plot_average_utility(i)
         evolution.plot_average_reputation(i)
@@ -144,10 +143,11 @@ if __name__ == "__main__":
         GameGrid(15, N, strategy_generator_instance, diagonal_neighbors=True), 
         utility_class=SimpleUtility,
         rep_class=ReputationManager,
-        asking_style="random",
+        asking_style="distributed",
         inverse_copy_prob=60,
         inverse_mutation_prob=1000,
         inverse_pardon_prob=200,
+        prob_power = 1.3,
         random_mutation=True)
 
     #Sweeper.sweep_rep_loss(np.arange(0.005, 0.105, 0.005 ), rounds=5000, repetitions=3, save_path="plots/sweeps/rep_loss_sweep4.png")